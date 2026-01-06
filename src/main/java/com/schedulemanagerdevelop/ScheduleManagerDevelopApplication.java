7f5d480864c1e370548876b755b5e269e6fb9463
김동진 <kimdj9790@naver.com>
2026-01-06 19:46:50 +0900

    setting

---
+++ src/main/java/com/schedulemanagerdevelop/ScheduleManagerDevelopApplication.java
@@ -0,0 +1,13 @@
+package com.schedulemanagerdevelop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScheduleManagerDevelopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScheduleManagerDevelopApplication.class, args);
+    }
+
+}
