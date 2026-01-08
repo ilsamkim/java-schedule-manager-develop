a2b9101640bc5fa325cee7ee5510747b71d7a803
김동진 <kimdj9790@naver.com>
2026-01-08 10:44:04 +0900

    pref: 메인 클래스 개선

--- src/main/java/com/schedulemanagerdevelop/ScheduleManagerDevelopApplication.java
+++ src/main/java/com/schedulemanagerdevelop/ScheduleManagerDevelopApplication.java
@@ -2,8 +2,11 @@ package com.schedulemanagerdevelop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class ScheduleManagerDevelopApplication {
 
     public static void main(String[] args) {
